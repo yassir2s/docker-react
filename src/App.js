665d18a305ed8e2f557e7e99edbb16c789d5052f
root <root@MB120554S.ad.si2m.tec>
2023-07-06 11:08:07 +0200

    added test App.js from feature branch

--- src/App.js
+++ src/App.js
@@ -9,13 +9,16 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          code from feature
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          change from feature branch
+          Learn React
         </a>
       </header>
     </div>
