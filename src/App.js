c1b2f3e19c6849207c76e9d578b83cdcbc49adf6
root <root@MB120554S.ad.si2m.tec>
2023-07-06 11:10:00 +0200

    added text App.js from feature branch

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-          code from feature
+          code from feature added
         </p>
         <a
           className="App-link"
